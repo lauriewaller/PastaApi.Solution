670855732a5b93e39ca5da7decc782e7ea550b3b
tlpackus <tlpackus@gmail.com>
2021-06-21 20:45:27 -0700

    practice scaffolding and update controller
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>
    Co-authored-by: Laurie Waller <lauriewaller7@gmail.com>

--- PastaApi/Controllers/PastasController.cs
+++ PastaApi/Controllers/PastasController.cs
@@ -1,97 +1,108 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PastaApi.Models;
-using System.Linq;
-
 
 namespace PastaApi.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class PastasController : ControllerBase
-  {
-    private readonly PastaApiContext _db;
-
-    public PastasController(PastaApiContext db)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PastasController : ControllerBase
     {
-      _db = db;
-    }
+        private readonly PastaApiContext _context;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Pasta>>> Get()
-    {
-      return await _db.Pastas.ToListAsync();
-    }
-    [HttpPost]
-    public async Task<ActionResult<Pasta>> Post(Pasta pasta)
-    {
-      _db.Pastas.Add(pasta);
-      await _db.SaveChangesAsync();
-
-      return CreatedAtAction("Post", new { id = pasta.PastaId }, pasta);
-    }
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Pasta>> GetPasta(int id)
-    {
-        var pasta = await _db.Pastas.FindAsync(id);
-
-        if (pasta == null)
+        public PastasController(PastaApiContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        return pasta;
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Pasta pasta)
-    {
-      if (id != pasta.PastaId)
-      {
-        return BadRequest();
-      }
-
-      _db.Entry(pasta).State = EntityState.Modified;
-
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!PastaExists(id))
+        // GET: api/Pastas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pasta>>> GetPastas()
         {
-          return NotFound();
+            return await _context.Pastas.ToListAsync();
         }
-        else
+
+        // GET: api/Pastas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pasta>> GetPasta(int id)
         {
-          throw;
+            var pasta = await _context.Pastas.FindAsync(id);
+
+            if (pasta == null)
+            {
+                return NotFound();
+            }
+
+            return pasta;
         }
-      }
 
-      return NoContent();
+        // PUT: api/Pastas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPasta(int id, Pasta pasta)
+        {
+            if (id != pasta.PastaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pasta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PastaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pastas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pasta>> PostPasta(Pasta pasta)
+        {
+            _context.Pastas.Add(pasta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPasta), new { id = pasta.PastaId }, pasta);
+        }
+
+        // DELETE: api/Pastas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePasta(int id)
+        {
+            var pasta = await _context.Pastas.FindAsync(id);
+            if (pasta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pastas.Remove(pasta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PastaExists(int id)
+        {
+            return _context.Pastas.Any(e => e.PastaId == id);
+        }
     }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletePasta(int id)
-    {
-      var pasta = await _db.Pastas.FindAsync(id);
-      if (pasta == null)
-      {
-        return NotFound();
-      }
-
-      _db.Pastas.Remove(pasta);
-      await _db.SaveChangesAsync();
-
-      return NoContent();
-    }
-
-    private bool PastaExists(int id)
-    {
-      return _db.Pastas.Any(e => e.PastaId == id);
-    }
-  }
 }
+
